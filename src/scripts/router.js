7c28f30449f52bc0d8680c9025bc3f653a45b6aa
sultanassi95 <mrsultanassi@gmail.com>
2017-12-19 15:16:06 +0200

    preparing backend

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -1,11 +1,34 @@
 const handlers = require('./handler.js');
 
 const router = (req, res) => {
-  const endpoint = req.url.split('/')[1];
+  const endpoint = req.url;
   if (endpoint === '') {
     handlers.HomePage(req, res);
-  } else if (endpoint === 'public') {
+  } else if (endpoint.startsWith('/public/') {
     handlers.generic(req, res);
-  } 
+  } else if (endpoint === '/login/') {
+    handlers.login(req, res);
+  } else if (endpoint === '/profile/') {
+    handlers.profiles(req, res);
+  } else if (endpoint === '/add_post/') {
+    handlers.addPost(req, res);
+  } else if (endpoint === '/edit_post/') {
+    handlers.editPost(req, res);
+  } else if (endpoint === '/like_post/') {
+    handlers.likePost(req, res);
+  } else if (endpoint === '/delete_post/') {
+    handlers.deletePost(req, res);
+  } else if (endpoint === '/comment/') {
+    handlers.comment(req, res);
+  } else if (endpoint === '/admin_panel/') {
+    handlers.adminPanel(req, res);
+  } else if (endpoint === '/add_user/') {
+    handlers.addUser(req, res);
+  } else if (endpoint === '/delete_user/') {
+    handlers.deleteUser(req, res);
+  } else {
+    res.writeHead(404, {'Content-Type':'text/html'});
+    res.end(`<h1> Path doesn't exist.</h1>`);
+  }
 };
 module.exports = router;
--- src/scripts/server.js
+++ src/scripts/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const router = require('./router');
 
 const server = http.createServer(router);
-const port = process.env.PORT || 6060;
+const port = process.env.PORT || 4060;
 
 server.listen(port, () => {
   console.log(`Magic Happen on port ${port}`);
